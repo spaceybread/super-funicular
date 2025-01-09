6363097236cf6b2ce77dbc0e08e7caa29ad0cbb7
spaceybread <spaceybread@gmail.com>
2025-01-10 03:58:43 +0530

    trying another approach

---
+++ simple_elevator.py
@@ -0,0 +1,49 @@
+# me when I realise data structures exist
+from elevator import *
+
+targets = set()
+next_targets = [] # prio queue for requests in opposite direction
+missed_dir_requests = [] # prio queue for requests in the same direction but not at an appropriate floor
+
+current_dir_requests = {} # hash map with request objects
+
+lowest, highest = 0, 4
+events = [None, Request(4, -1, 0), None, None, None, None, None, None, None, None, Request(2, 1, 4), None, None, None, None, None, None, None, None, None]
+elevator = Cart()
+
+# for now, I'm assuming it takes negligible time to enter/exit a floor within a time step
+# I will probaly change this later, implementing two different 'time' mechanisms with real time
+# and time step but for now, I don't really want to deal with it
+
+for event in events:
+    cur_floor = elevator.get_floor()
+    
+    # if the current floor is one of the stops
+    if cur_floor in targets:
+        # do that stop
+        targets.remove(cur_floor)
+        
+        # if this stop was part of a request's initial call
+        if cur_floor in current_dir_requests:
+            reqs = current_dir_requests.get(cur_floor)
+            
+            # add all the target floors
+            for re in reqs:
+                targets.add(re.get_target_floor(elevator))
+    
+    # if targets have been cleared and there are no more requests, relax
+    if len(targets) == 0 and len(next_targets) == 0 and len(missed_dir_requests) == 0:
+        elevator.set_state(0)
+
+    if event == None:
+        elevator.update_position()
+        continue
+    
+    floor, direction = event.get_initial_request()
+    
+    if elevator.get_status() == 0:
+        targets.add(floor)
+        elevator.set_state(direction)
+        
+        if floor in current_dir_requests: current_dir_requests[floor].append(event)
+        else: current_dir_requests[floor] = [event]
