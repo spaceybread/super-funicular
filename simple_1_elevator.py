752a7e243e7fae8bc61f186c96d8c4a494be0b25
spaceybread <spaceybread@gmail.com>
2025-01-10 01:41:18 +0530

    handles simple spaced out queries

--- simple_1_elevator.py
+++ simple_1_elevator.py
@@ -5,7 +5,7 @@ from elevator import *
 
 lowest, highest = 0, 4
 # events = [None, Request(2, 1, 4), None, Request(3, 1, 4), None, None, Request(4, -1, 0)]
-events = [None, Request(2, 1, 4), None, None, None, None, None]
+events = [None, Request(2, 1, 4), None, None, None, None, None, None, None, None, Request(2, 1, 4), None, None, None, None, None, None, None, None, None]
 elevator = Cart()
 
 # think of events as any command issued to the elevator from outside the elevator
@@ -14,13 +14,25 @@ elevator = Cart()
 # an elevator cart, this is not entirely realistic but I'm running with it for now
 
 TARGET = None
-LOG = []
+assign_queue = []
 
 for event in events:
-    print("Floor:", elevator.get_floor(), "State:", elevator.get_state())
+    print("Floor:", elevator.get_floor(), "| State:", elevator.get_state(), "| In Request:", elevator.get_request_status())
     if event == None:
         # empty time step, move if required
-        if elevator.get_floor() == TARGET: elevator.update_state(0)
+        if elevator.get_floor() == TARGET:
+            elevator.update_state(0)
+            
+            if not elevator.get_request_status() and len(assign_queue) > 0:
+                queue_req = elevator.get_requests()[assign_queue[-1]]
+                TARGET = queue_req.get_target_floor(elevator)
+                elevator.update_state(queue_req.get_initial_request()[1])
+                elevator.flip_in_request()
+                continue
+            elif len(assign_queue) > 0:
+                assign_queue.pop(0)
+                elevator.flip_in_request()
+            
         elevator.update_position()
         continue
     
@@ -28,13 +40,13 @@ for event in events:
         # cart is unassigned and can be assinged this event
         floor, dir = event.get_initial_request()
         ele_floor = elevator.get_floor()
-        elevator.flip_in_request()
+        assign_queue.append(elevator.add_request(event))
         
         if floor == ele_floor:
             # if the cart is already at the floor
             elevator.update_state(dir)
             TARGET = event.get_target_floor(elevator)
-            # don't update the position,
+            elevator.flip_in_request()
             continue
         
         else:
